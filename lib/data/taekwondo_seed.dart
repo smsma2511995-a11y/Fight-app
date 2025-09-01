675ec15a860dd67c6b09ce2d51bf8ebeeb2753ea
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-01 17:27:12 +0300

    Create taekwondo_seed.dart

---
+++ lib/data/taekwondo_seed.dart
@@ -0,0 +1,112 @@
+import '../models/exercise_model.dart';
+
+final List<Exercise> taekwondoExercises = [
+  Exercise(
+    name: 'ركلة أمامية (Ap Chagi)',
+    description: 'رفع الركبة ثم دفع الكرة الأمامية للقدم في خط مستقيم.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'ركلة راوندهاوس (Dollyo Chagi)',
+    description: 'تدوير الورك ولف الساق لتوجيه الضربة على خط دائري.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'ركلة جانبية (Yop Chagi)',
+    description: 'رفع الركبة جانبيًا ثم الدفع بالكعب نحو الهدف.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/14/Kumite_bracket.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'ركلة خلفية (Dwi Chagi)',
+    description: 'التفاتة سريعة ونظر للهدف ثم دفع بالكعب للخلف.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/14/Kumite_bracket.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'ركلة الفأس (Naeryo Chagi)',
+    description: 'رفع الساق عاليًا ثم الإسقاط لأسفل بتحكم على الهدف.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'ركلة خطافية (Hook Kick)',
+    description: 'مرجحة الساق للخارج ثم خطف الكعب للداخل على مسار قوسي.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'هلالية للخارج (Bakkat/Bandal Chagi)',
+    description: 'قوس من الداخل للخارج لاستهداف المستوى العالي.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'هلالية للداخل (An Chagi)',
+    description: 'قوس من الخارج للداخل مع توازن الجذع واليدين.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'ركلة دفع (Teep/Push Kick)',
+    description: 'دفع مباشر لإبعاد الخصم وخلق مسافة وسيطرة على الإيقاع.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/14/Kumite_bracket.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'باك سبينج (Spinning Back Kick)',
+    description: 'دوران 180° ثم دفع بالكعب على خط مستقيم للخلف.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/14/Kumite_bracket.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'تورنادو كيك (Tornado)',
+    description: 'قفزة مع دوران وتبديل القدمين لإضافة زخم قبل الركل.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/14/Kumite_bracket.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'قفزة أمامية',
+    description: 'إضافة قفزة لركلة أمامية لزيادة المدى والزخم.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/14/Kumite_bracket.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'قفزة جانبية',
+    description: 'تنفيذ يوب تشاجي مع قفزة خفيفة لقطع المسافة بسرعة.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/14/Kumite_bracket.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'قفزة خلفية',
+    description: 'إضافة قفزة إلى دوي تشاجي مع تحكم في الهبوط والتوازن.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/14/Kumite_bracket.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: '360 راوند',
+    description: 'دوران كامل مع راوندهاوس عالية وتحكم بالورك أثناء الطيران.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'سيف اليد (Sonnal)',
+    description: 'ضربة بحافة اليد مع شد الأصابع وتثبيت المعصم.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'صد علوي',
+    description: 'حاجز تصاعدي لحماية الرأس وفتح زاوية للهجوم المقابل.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'تايكوندو',
+  ),
+  Exercise(
+    name: 'صد سفلي',
+    description: 'حاجز لأسفل لإبعاد الركلات المنخفضة والمحافظة على التوازن.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'تايكوندو',
+  ),
+];
