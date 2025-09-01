9b73bda46263573be09a6f565b9724b39ae9b43c
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-01 18:45:39 +0300

    Create kickboxing_seed.dart

---
+++ lib/data/kickboxing_seed.dart
@@ -0,0 +1,34 @@
+import '../models/exercise_model.dart';
+
+final List<MartialArtsExercise> kickboxingExercises = [
+  MartialArtsExercise(
+    name: "Front Kick",
+    description: "ركلة أمامية سريعة باستخدام القدم الأمامية أو الخلفية لدفع الخصم.",
+    gifUrl: "https://media.tenor.com/rFJcEl4B4ogAAAAC/front-kick-martial-arts.gif",
+    calories: 6,
+  ),
+  MartialArtsExercise(
+    name: "Roundhouse Kick",
+    description: "ركلة دائرية قوية تستهدف الرأس أو الجسم باستخدام الساق الخلفية.",
+    gifUrl: "https://media.tenor.com/wWvX_QO_H8sAAAAC/roundhouse-kick.gif",
+    calories: 8,
+  ),
+  MartialArtsExercise(
+    name: "Side Kick",
+    description: "ركلة جانبية قوية لدفع الخصم والحفاظ على المسافة.",
+    gifUrl: "https://media.tenor.com/ZhdKZ5lI1qQAAAAC/sidekick-kickboxing.gif",
+    calories: 7,
+  ),
+  MartialArtsExercise(
+    name: "Punch-Kick Combo",
+    description: "مزيج من اللكمات السريعة يتبعها ركلة أمامية أو جانبية.",
+    gifUrl: "https://media.tenor.com/zVPK6L45-lEAAAAC/kickboxing-combo.gif",
+    calories: 9,
+  ),
+  MartialArtsExercise(
+    name: "Defensive Footwork",
+    description: "حركات قدم سريعة لتفادي الهجمات والحفاظ على التوازن.",
+    gifUrl: "https://media.tenor.com/zLg3OdoJxI8AAAAC/footwork-kickboxing.gif",
+    calories: 5,
+  ),
+];
