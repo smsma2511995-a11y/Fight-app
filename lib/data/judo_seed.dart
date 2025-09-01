4a7bd3b9e807031ffea421a366e906ea257cf9d2
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-01 17:25:38 +0300

    Create judo_seed.dart

---
+++ lib/data/judo_seed.dart
@@ -0,0 +1,112 @@
+import '../models/exercise_model.dart';
+
+final List<Exercise> judoExercises = [
+  Exercise(
+    name: 'أوسوتو جاري (Osoto Gari)',
+    description: 'إسقاط الساق الكبرى من الخارج بعد كسر التوازن للأمام-الجانب.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'أو-جوشي (O-Goshi)',
+    description: 'رمية الورك الكبرى برفع الخصم على الورك ثم الدوران.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'إبّون سيوي ناجي (Ippon Seoi Nage)',
+    description: 'رمية الكتف بذراع واحدة مع سحب الذراع والتدوير للأسفل.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'موروته سيوي ناجي (Morote Seoi Nage)',
+    description: 'نسخة بذراعين من رمية الكتف لرافعة أقوى وثبات أعلى.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'هاراي جوشي (Harai Goshi)',
+    description: 'كنس الورك مع دفع الورك للداخل وكنس الساق.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'أوتشي ماتا (Uchi Mata)',
+    description: 'رفس الفخذ من الداخل مع محور على الورك لإسقاط أمامي-جانبي.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'تاي أوتوشي (Tai Otoshi)',
+    description: 'إسقاط الجسم مع عرقلة الساق الأمامية وتدوير الذراعين.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'كوتشي جاري (Kouchi Gari)',
+    description: 'حصد الساق الصغيرة من الداخل بعد سحب الخصم للأمام.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'أوتشي جاري (Ouchi Gari)',
+    description: 'حصد الساق الكبيرة من الداخل مع دفع الكتف والياقة.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'ديه-أشي باراي (De Ashi Barai)',
+    description: 'كنس القدم المتقدمة لحظة حمل الوزن عليها.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'سساي تسوريكومي آشي',
+    description: 'عرقلة القدم مع سحب ورفع الياقة لتغيير التوازن.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'كوشي جورما (Koshi Guruma)',
+    description: 'طوق الرأس والدوران حول الورك لرمي دائري.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/7/78/Judo_Morote_Seoi_Nage_by_Bryan.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'توموي ناجي (Tomoe Nage)',
+    description: 'رمية دائرية باستخدام القدم كنقطة ارتكاز من وضعية السحب للأمام.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'سومي غايشي (Sumi Gaeshi)',
+    description: 'قلاب زاوي للخلف مع خطاف بالقدم ولف للجذع.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'كيسا-جاتامي (Kesa Gatame)',
+    description: 'تثبيت جانبي مع تطويق الرأس والذراع والتحكم بالوزن.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'يوكو شيهو-جاتامي',
+    description: 'تثبيت جانبي بأربع نقاط مع سيطرة على الكتفين والحوض.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'تاتي شيهو-جاتامي',
+    description: 'تثبيت طولي من الأعلى مع توزيع وزن مركزي.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'جودو',
+  ),
+  Exercise(
+    name: 'أوشيرو كيسا-جاتامي',
+    description: 'تثبيت الكتف العكسي مع إغلاق الفراغات بالورك والركب.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Jud-seiza.gif',
+    category: 'جودو',
+  ),
+];
