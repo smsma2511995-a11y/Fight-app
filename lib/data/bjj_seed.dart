ba756742542313ac7c90c3f567892296244f0489
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-01 18:48:27 +0300

    Create bjj_seed.dart

---
+++ lib/data/bjj_seed.dart
@@ -0,0 +1,34 @@
+import '../models/exercise_model.dart';
+
+final List<MartialArtsExercise> bjjExercises = [
+  MartialArtsExercise(
+    name: "Guard Pass",
+    description: "المرور من حراسة الخصم للسيطرة على وضعية جانبية أو علوية.",
+    gifUrl: "https://media.tenor.com/3t-82xjZsvsAAAAC/bjj-guard-pass.gif",
+    calories: 7,
+  ),
+  MartialArtsExercise(
+    name: "Triangle Choke",
+    description: "خنق باستخدام الساقين لتقييد رقبة وذراع الخصم.",
+    gifUrl: "https://media.tenor.com/NUtV21OX5G0AAAAC/bjj-triangle.gif",
+    calories: 9,
+  ),
+  MartialArtsExercise(
+    name: "Armbar",
+    description: "ضغط على مفصل الكوع لخصمك باستخدام الورك.",
+    gifUrl: "https://media.tenor.com/99uqfTDoa8sAAAAC/bjj-armbar.gif",
+    calories: 8,
+  ),
+  MartialArtsExercise(
+    name: "Rear Naked Choke",
+    description: "خنق من الخلف باستخدام الذراعين للتحكم في التنفس.",
+    gifUrl: "https://media.tenor.com/Rf6jL2GbJ6EAAAAC/rear-naked-choke.gif",
+    calories: 10,
+  ),
+  MartialArtsExercise(
+    name: "Shrimp Escape",
+    description: "حركة للهروب من الضغط الأرضي باستخدام الورك.",
+    gifUrl: "https://media.tenor.com/ttZ3-x5GH2oAAAAC/bjj-shrimp.gif",
+    calories: 6,
+  ),
+];
