287c18c2283fc3d896e1d31dc98af2e5128076e0
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-01 18:23:17 +0300

    Update exercises_screen.dart

--- lib/screens/exercises_screen.dart
+++ lib/screens/exercises_screen.dart
@@ -1,47 +1,40 @@
 import 'package:flutter/material.dart';
+import '../data/database_helper.dart';
+import '../models/exercise_model.dart';
 
-import 'package:fight_app/data/exercises_seed.dart';
-import 'package:fight_app/models/exercise_model.dart';
-// استيراد الشاشة الجديدة التي أنشأناها للتو
-import 'package:fight_app/screens/exercise_detail_screen.dart'; 
+class ExercisesScreen extends StatefulWidget {
+  @override
+  _ExercisesScreenState createState() => _ExercisesScreenState();
+}
 
-class ExercisesScreen extends StatelessWidget {
-  const ExercisesScreen({super.key});
+class _ExercisesScreenState extends State<ExercisesScreen> {
+  final dbHelper = DatabaseHelper();
+  List<MartialArtsExercise> exercises = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadExercises();
+  }
+
+  Future<void> _loadExercises() async {
+    final data = await dbHelper.getAllExercises();
+    setState(() {
+      exercises = data;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('قائمة التمارين'),
-      ),
+      appBar: AppBar(title: const Text("Martial Arts Exercises")),
       body: ListView.builder(
-        itemCount: allExercises.length,
-        itemBuilder: (BuildContext context, int index) {
-          final MartialArtsExercise exercise = allExercises[index];
-
-          return Card(
-            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
-            child: ListTile(
-              leading: Icon(Icons.fitness_center, color: Colors.red[700]),
-              title: Text(
-                exercise.nameAr,
-                style: const TextStyle(fontWeight: FontWeight.bold),
-              ),
-              subtitle: Text('${exercise.category} - ${exercise.difficulty}'),
-              trailing: const Icon(Icons.arrow_forward_ios),
-
-              // --- هذا هو التعديل الوحيد ---
-              // الآن عند الضغط، سينتقل المستخدم إلى شاشة التفاصيل
-              // وسيرسل معه بيانات التمرين الذي تم الضغط عليه
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ExerciseDetailScreen(exercise: exercise),
-                  ),
-                );
-              },
-            ),
+        itemCount: exercises.length,
+        itemBuilder: (context, index) {
+          final ex = exercises[index];
+          return ListTile(
+            title: Text(ex.name),
+            subtitle: Text(ex.description),
           );
         },
       ),
