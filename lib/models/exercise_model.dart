f054008457dc6ea09b744e22b601395f893e6202
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-01 18:20:11 +0300

    Update exercise_model.dart

--- lib/models/exercise_model.dart
+++ lib/models/exercise_model.dart
@@ -2,38 +2,34 @@ class MartialArtsExercise {
   final int id;
   final String name;
   final String description;
-  final int duration; // بالثواني
+  final String category; // Karate, Judo, etc.
   final int calories;
-  final String category; // Karate, Judo, Taekwondo, ...
 
-  const MartialArtsExercise({
+  MartialArtsExercise({
     required this.id,
     required this.name,
     required this.description,
-    required this.duration,
-    required this.calories,
     required this.category,
+    required this.calories,
   });
-
-  factory MartialArtsExercise.fromJson(Map<String, dynamic> json) {
-    return MartialArtsExercise(
-      id: json['id'],
-      name: json['name'],
-      description: json['description'],
-      duration: json['duration'],
-      calories: json['calories'],
-      category: json['category'],
-    );
-  }
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'name': name,
       'description': description,
-      'duration': duration,
-      'calories': calories,
       'category': category,
+      'calories': calories,
     };
+  }
+
+  factory MartialArtsExercise.fromMap(Map<String, dynamic> map) {
+    return MartialArtsExercise(
+      id: map['id'],
+      name: map['name'],
+      description: map['description'],
+      category: map['category'],
+      calories: map['calories'],
+    );
   }
 }
