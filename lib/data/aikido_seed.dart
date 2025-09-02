3e122fe07bef93dddf0f302b8bd7518963151e9c
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-02 16:08:23 +0300

    Create aikido_seed.dart

---
+++ lib/data/aikido_seed.dart
@@ -0,0 +1,66 @@
+final Map<String, dynamic> aikido = {
+  "name": "Aikido",
+  "description": "Aikido هو فن قتالي ياباني يركز على توازن الجسم، لفّات، سقوط آمن، وتقنيات السيطرة على الخصم باستخدام طاقة الخصم.",
+  "exercises": [
+    {
+      "name": "Tenkan (التدوير)",
+      "description": "حركة تدوير الجسم لتغيير الاتجاه أثناء الهجوم، مع الحفاظ على التوازن.",
+      "video": "https://www.youtube.com/watch?v=5emQtbU0-mA",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Irimi (الدخول المباشر)",
+      "description": "الاقتراب المباشر من الخصم لتفادي هجومه وتطبيق تقنية السيطرة.",
+      "video": "https://www.youtube.com/watch?v=5emQtbU0-mA",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Kaiten Nage (الرمي الدوار)",
+      "description": "تقنية رمي حيث يتم تدوير الخصم حول محوره وإسقاطه على الأرض.",
+      "video": "https://www.youtube.com/watch?v=5emQtbU0-mA",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Kotegaeshi (الاستدارة العكسية)",
+      "description": "تقنية لفّ المعصم لتوجيه الخصم نحو الأرض باستخدام قوته ضده.",
+      "video": "https://www.youtube.com/watch?v=5emQtbU0-mA",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Shihonage (الرمي الرباعي)",
+      "description": "تقنية رمي حيث يتم تدوير الخصم في أربع اتجاهات مختلفة لإسقاطه.",
+      "video": "https://www.youtube.com/watch?v=5emQtbU0-mA",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Sankyo (القبضة الثلاثية)",
+      "description": "تقنية تحكم في المعصم والذراع لتوجيه الخصم نحو الأرض.",
+      "video": "https://www.youtube.com/watch?v=5emQtbU0-mA",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Nikyo (القبضة الثنائية)",
+      "description": "تقنية تحكم في المعصم والذراع باستخدام الضغط لتوجيه الخصم.",
+      "video": "https://www.youtube.com/watch?v=5emQtbU0-mA",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Ude Garami (اللف الذراعي)",
+      "description": "تقنية لفّ الذراع لتقييد حركة الخصم.",
+      "video": "https://www.youtube.com/watch?v=5emQtbU0-mA",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Koshi Nage (الرمي الحوضي)",
+      "description": "تقنية رمي حيث يتم استخدام الحوض لرفع الخصم وإسقاطه.",
+      "video": "https://www.youtube.com/watch?v=5emQtbU0-mA",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Zanshin (الوعي الكامل)",
+      "description": "حالة من الوعي الكامل بالمحيط والتركيز أثناء التدريب.",
+      "video": "https://www.youtube.com/watch?v=5emQtbU0-mA",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    }
+  ]
+};
