dbc0445fd57cc05d2a85628c29807f6463ec9532
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-02 16:16:00 +0300

    Create flexibility_seed.dart

---
+++ lib/data/flexibility_seed.dart
@@ -0,0 +1,66 @@
+final Map<String, dynamic> flexibility = {
+  "name": "Flexibility",
+  "description": "تمارين المرونة لتحسين نطاق الحركة في المفاصل والعضلات، تساعد على الوقاية من الإصابات وزيادة الأداء البدني.",
+  "exercises": [
+    {
+      "name": "Forward Bend",
+      "description": "انحناء للأمام لتمديد أسفل الظهر وأوتار الركبة.",
+      "video": "https://www.youtube.com/watch?v=vG9X6Nwqz1U",
+      "gif": "https://media.giphy.com/media/l0HlU7iKkp1o/giphy.gif"
+    },
+    {
+      "name": "Cat-Cow Stretch",
+      "description": "تمرين للظهر والعمود الفقري لتحسين المرونة والتوازن.",
+      "video": "https://www.youtube.com/watch?v=kqnua4rHVVA",
+      "gif": "https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif"
+    },
+    {
+      "name": "Hip Flexor Stretch",
+      "description": "تمدد عضلات الورك الأمامية لتحسين حركة الحوض والفخذ.",
+      "video": "https://www.youtube.com/watch?v=KY-lA9DgKXA",
+      "gif": "https://media.giphy.com/media/l0HlJgk2v4/giphy.gif"
+    },
+    {
+      "name": "Shoulder Stretch",
+      "description": "تمرين لتمديد عضلات الكتف وتحسين نطاق الحركة.",
+      "video": "https://www.youtube.com/watch?v=QNKqgplJzE0",
+      "gif": "https://media.giphy.com/media/3o6ZsWX0H2M/giphy.gif"
+    },
+    {
+      "name": "Hamstring Stretch",
+      "description": "تمدد أوتار الركبة لتحسين المرونة وتقليل خطر الإصابات.",
+      "video": "https://www.youtube.com/watch?v=ZxKqkbk1pg0",
+      "gif": "https://media.giphy.com/media/l0HlT6UqI8/giphy.gif"
+    },
+    {
+      "name": "Quadriceps Stretch",
+      "description": "تمديد عضلات الفخذ الأمامية لتحسين الحركة أثناء الركض والقفز.",
+      "video": "https://www.youtube.com/watch?v=K6M5v2H4F9c",
+      "gif": "https://media.giphy.com/media/3o6ZsWJ1H2M/giphy.gif"
+    },
+    {
+      "name": "Side Bend",
+      "description": "انحناء جانبي لتمديد العضلات الجانبية للظهر والبطن.",
+      "video": "https://www.youtube.com/watch?v=3Dqr5Kc5XkE",
+      "gif": "https://media.giphy.com/media/l0HlP1k2/giphy.gif"
+    },
+    {
+      "name": "Spinal Twist",
+      "description": "لف العمود الفقري لتحسين مرونة الظهر والرقبة.",
+      "video": "https://www.youtube.com/watch?v=5RtW8s9k2Fs",
+      "gif": "https://media.giphy.com/media/3o6ZsWQq/giphy.gif"
+    },
+    {
+      "name": "Butterfly Stretch",
+      "description": "تمدد داخلي للفخذين لتحسين المرونة في الورك والفخذ.",
+      "video": "https://www.youtube.com/watch?v=V9V8kD7Z8k4",
+      "gif": "https://media.giphy.com/media/l0HlP1k3/giphy.gif"
+    },
+    {
+      "name": "Calf Stretch",
+      "description": "تمديد عضلات الساق الخلفية لتحسين المرونة ومنع الشد العضلي.",
+      "video": "https://www.youtube.com/watch?v=Yd1T3K5G4l4",
+      "gif": "https://media.giphy.com/media/3o6ZsXQk/giphy.gif"
+    }
+  ]
+};
