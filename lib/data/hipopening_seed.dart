3dabb075b4c37d99468e74cede9464ba2b1fd679
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-02 16:18:20 +0300

    Create hipopening_seed.dart

---
+++ lib/data/hipopening_seed.dart
@@ -0,0 +1,66 @@
+final Map<String, dynamic> hipOpening = {
+  "name": "Hip Opening",
+  "description": "تمارين فعّالة لفتح الحوض وزيادة المرونة، تقوي الركبتين، الوركين، وتزيد قوة الضربات والتحركات في جميع فنون القتال.",
+  "exercises": [
+    {
+      "name": "Butterfly Stretch",
+      "description": "اجلس وضم قدميك معًا وحرك الركبتين للأعلى والأسفل لتوسيع الوركين.",
+      "video": "https://www.youtube.com/watch?v=V9V8kD7Z8k4",
+      "gif": "https://media.giphy.com/media/l0HlP1k3/giphy.gif"
+    },
+    {
+      "name": "Pigeon Pose",
+      "description": "وضعية الحمامة لتمديد الورك الأمامي والخلفي.",
+      "video": "https://www.youtube.com/watch?v=lxK3XK7jLOQ",
+      "gif": "https://media.giphy.com/media/3o6ZsWQk/giphy.gif"
+    },
+    {
+      "name": "Lunge with Twist",
+      "description": "تمرين الطعن مع التواء الجسم لفتح الوركين وتقوية العضلات الأساسية.",
+      "video": "https://www.youtube.com/watch?v=KY-lA9DgKXA",
+      "gif": "https://media.giphy.com/media/l0HlJgk2v4/giphy.gif"
+    },
+    {
+      "name": "Frog Stretch",
+      "description": "تمدد داخلي للفخذين لتحريك الوركين وزيادة المرونة.",
+      "video": "https://www.youtube.com/watch?v=kqhJq37Nm-U",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Hip Circles",
+      "description": "تحريك الوركين في دوائر لتليين المفاصل وتحسين الحركة.",
+      "video": "https://www.youtube.com/watch?v=5S7y-ZZZw0Q",
+      "gif": "https://media.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+    },
+    {
+      "name": "Seated Straddle Stretch",
+      "description": "اجلس وافتح الساقين قدر الإمكان ثم انحن للأمام لتحريك الوركين والفخذين.",
+      "video": "https://www.youtube.com/watch?v=2ZstRO2g8h4",
+      "gif": "https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif"
+    },
+    {
+      "name": "Frog Squat",
+      "description": "القرفصاء العميقة مع وضع اليدين للأرض لفتح الوركين وتقوية الفخذين.",
+      "video": "https://www.youtube.com/watch?v=lhbE7oRocS0",
+      "gif": "https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif"
+    },
+    {
+      "name": "Standing Hip Circles",
+      "description": "حركة دائرية للوركين أثناء الوقوف لتدفئة المفاصل وزيادة المرونة.",
+      "video": "https://www.youtube.com/watch?v=f1Q3Rr0p1xI",
+      "gif": "https://media.giphy.com/media/3o6ZsWX0H2M/giphy.gif"
+    },
+    {
+      "name": "Lizard Stretch",
+      "description": "تمديد الورك الأمامي مع دعم اليدين على الأرض لتحريك الورك والفخذين.",
+      "video": "https://www.youtube.com/watch?v=zG5LR-sN7Po",
+      "gif": "https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif"
+    },
+    {
+      "name": "Firelog Pose",
+      "description": "وضعية جلوس لتوسيع الوركين والجزء الداخلي للفخذين بعمق.",
+      "video": "https://www.youtube.com/watch?v=MPH2XEx5s0o",
+      "gif": "https://media.giphy.com/media/l0HlP1k2v4/giphy.gif"
+    }
+  ]
+};
