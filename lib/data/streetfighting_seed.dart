306ec1bf54317ab6d20ca372465304de1d90fb91
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-02 16:11:23 +0300

    Create streetfighting_seed.dart

---
+++ lib/data/streetfighting_seed.dart
@@ -0,0 +1,66 @@
+final Map<String, dynamic> streetFighting = {
+  "name": "Street Fighting",
+  "description": "تعلم الدفاع عن النفس في مواقف الشارع، يشمل الضربات السريعة، الهروب، واستخدام البيئة المحيطة لصالحك.",
+  "exercises": [
+    {
+      "name": "Straight Punch",
+      "description": "لكمة أمامية سريعة تستخدم لتوقيف الخصم أو كفتح لمزيد من الهجمات.",
+      "video": "https://www.youtube.com/watch?v=r2KtH65reOg",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Front Kick",
+      "description": "ركلة أمامية لتباعد الخصم عنك أو لتوجيهه بعيدًا عن الهجوم.",
+      "video": "https://www.youtube.com/watch?v=kWPXXPNN9Ts",
+      "gif": "https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif"
+    },
+    {
+      "name": "Elbow Strike",
+      "description": "ضربة المرفق فعّالة في المسافات القريبة لإحداث صدمة للخصم.",
+      "video": "https://www.youtube.com/watch?v=ndLfek1lruA",
+      "gif": "https://media.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+    },
+    {
+      "name": "Knee Strike",
+      "description": "ضربة الركبة للخصم في المناطق الحيوية مثل البطن أو الفخذ.",
+      "video": "https://www.youtube.com/watch?v=r2KtH65reOg",
+      "gif": "https://media.giphy.com/media/l41lFw057lAJQMwg0/giphy.gif"
+    },
+    {
+      "name": "Head Movement",
+      "description": "تحريك الرأس لتجنب الضربات والهجمات القادمة.",
+      "video": "https://www.youtube.com/watch?v=kWPXXPNN9Ts",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Duck & Roll",
+      "description": "انحناء وتدحرج لتفادي الضربات أو الاختفاء من الهجوم.",
+      "video": "https://www.youtube.com/watch?v=ndLfek1lruA",
+      "gif": "https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif"
+    },
+    {
+      "name": "Grab Escape",
+      "description": "تقنيات لتحرير نفسك من الإمساك بالذراع أو الملابس.",
+      "video": "https://www.youtube.com/watch?v=r2KtH65reOg",
+      "gif": "https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif"
+    },
+    {
+      "name": "Side Kick",
+      "description": "ركلة جانبية لتوجيه الخصم بعيدًا عنك أو لإحداث صدمة.",
+      "video": "https://www.youtube.com/watch?v=kWPXXPNN9Ts",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Palm Strike",
+      "description": "ضربة كف اليد للوجه أو الصدر لإرباك الخصم بسرعة.",
+      "video": "https://www.youtube.com/watch?v=ndLfek1lruA",
+      "gif": "https://media.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+    },
+    {
+      "name": "Environmental Use",
+      "description": "استخدام البيئة المحيطة مثل الجدران، الطاولات، أو العصا للدفاع والهجوم.",
+      "video": "https://www.youtube.com/watch?v=r2KtH65reOg",
+      "gif": "https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif"
+    }
+  ]
+};
