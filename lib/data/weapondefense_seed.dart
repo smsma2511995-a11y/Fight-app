377a782cdd68ead77ca9b753ea4a1356b8a4dd9a
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-02 16:14:51 +0300

    Create weapondefense_seed.dart

---
+++ lib/data/weapondefense_seed.dart
@@ -0,0 +1,66 @@
+final Map<String, dynamic> weaponDefense = {
+  "name": "Weapon Defense",
+  "description": "تمارين الدفاع ضد الأسلحة المختلفة مثل السكاكين والعصي، لتعلم التحرك، التفادي، والسيطرة على المهاجم.",
+  "exercises": [
+    {
+      "name": "Knife Defense - Basic Block",
+      "description": "تعلم كيفية حجب السكين باستخدام اليدين أو الذراعين بطريقة آمنة.",
+      "video": "https://www.youtube.com/watch?v=5QZbJw4-PoA",
+      "gif": "https://media.giphy.com/media/l0HlNQ03J5JxX6lva/giphy.gif"
+    },
+    {
+      "name": "Stick Defense - Basic Parry",
+      "description": "التصدي للهجمات بالعصا باستخدام حركات التهديد والصد.",
+      "video": "https://www.youtube.com/watch?v=8m8Rk8lYX8g",
+      "gif": "https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif"
+    },
+    {
+      "name": "Knife Disarm Technique 1",
+      "description": "تقنية السيطرة على السكين من الخصم وإخراجه بأمان.",
+      "video": "https://www.youtube.com/watch?v=2gq6KxAv1z4",
+      "gif": "https://media.giphy.com/media/l0HlNQ03J5JxX6lva/giphy.gif"
+    },
+    {
+      "name": "Stick Disarm Technique",
+      "description": "طريقة أخذ العصا من المهاجم مع الحفاظ على التوازن.",
+      "video": "https://www.youtube.com/watch?v=Y3iXpOzCPOU",
+      "gif": "https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif"
+    },
+    {
+      "name": "Knife Defense - Step Back",
+      "description": "الخطوة إلى الخلف لتجنب الطعنات السريعة.",
+      "video": "https://www.youtube.com/watch?v=5QZbJw4-PoA",
+      "gif": "https://media.giphy.com/media/l0HlNQ03J5JxX6lva/giphy.gif"
+    },
+    {
+      "name": "Stick Defense - Side Step",
+      "description": "التحرك جانبياً لتفادي الهجمات وتغيير زاوية الخصم.",
+      "video": "https://www.youtube.com/watch?v=8m8Rk8lYX8g",
+      "gif": "https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif"
+    },
+    {
+      "name": "Knife Defense - Grab & Twist",
+      "description": "الإمساك بمعصم الخصم مع توجيه السكين بعيداً.",
+      "video": "https://www.youtube.com/watch?v=2gq6KxAv1z4",
+      "gif": "https://media.giphy.com/media/l0HlNQ03J5JxX6lva/giphy.gif"
+    },
+    {
+      "name": "Stick Defense - Block & Counter",
+      "description": "صد الضربة بالعصا ثم الرد بهجوم مضاد سريع.",
+      "video": "https://www.youtube.com/watch?v=Y3iXpOzCPOU",
+      "gif": "https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif"
+    },
+    {
+      "name": "Knife Defense - Low Sweep",
+      "description": "سحق الركبة لتغيير توازن الخصم وإبطال هجوم السكين.",
+      "video": "https://www.youtube.com/watch?v=5QZbJw4-PoA",
+      "gif": "https://media.giphy.com/media/l0HlNQ03J5JxX6lva/giphy.gif"
+    },
+    {
+      "name": "Stick Defense - High Parry",
+      "description": "صد الضربات العالية بالعصا مع الحفاظ على خط الدفاع.",
+      "video": "https://www.youtube.com/watch?v=8m8Rk8lYX8g",
+      "gif": "https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif"
+    }
+  ]
+};
