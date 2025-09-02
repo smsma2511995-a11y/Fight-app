867629ec5d1917fcc7ebb3afed300f754d8415a7
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-02 16:40:02 +0300

    Update martial_exercises.dart

--- lib/data/martial_exercises.dart
+++ lib/data/martial_exercises.dart
@@ -1,11 +1,34 @@
 import '../models/exercise_model.dart';
+import 'aikido_seed.dart';
+import 'bjj_seed.dart';
+import 'boxing_seed.dart';
 import 'karate_seed.dart';
 import 'judo_seed.dart';
+import 'kickboxing_seed.dart';
+import 'kungfu_seed.dart';
+import 'muaythai_seed.dart';
+import 'musclebuilding_seed.dart';
+import 'streetfighting_seed.dart';
 import 'taekwondo_seed.dart';
+import 'weapondefense_seed.dart';
+import 'wrestling_seed.dart';
+import 'flexibility_seed.dart';
+import 'hipopening_seed.dart';
 
-// ضيف باقي الملفات هنا لاحقًا لما نبعتها لك
 final List<Exercise> allExercises = [
+  ...aikidoExercises,
+  ...bjjExercises,
+  ...boxingExercises,
   ...karateExercises,
   ...judoExercises,
+  ...kickboxingExercises,
+  ...kungfuExercises,
+  ...muaythaiExercises,
+  ...muscleBuildingExercises,
+  ...streetFightingExercises,
   ...taekwondoExercises,
+  ...weaponDefenseExercises,
+  ...wrestlingExercises,
+  ...flexibilityExercises,
+  ...hipOpeningExercises,
 ];
