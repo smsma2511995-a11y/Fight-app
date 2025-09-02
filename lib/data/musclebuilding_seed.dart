9f2925611870c103afb60bd70d5daff1173020db
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-02 16:12:48 +0300

    Create musclebuilding_seed.dart

---
+++ lib/data/musclebuilding_seed.dart
@@ -0,0 +1,66 @@
+final Map<String, dynamic> muscleBuilding = {
+  "name": "Muscle Building",
+  "description": "تمارين القوة الأساسية لتقوية العضلات وزيادة الكتلة العضلية باستخدام أوزان أو وزن الجسم.",
+  "exercises": [
+    {
+      "name": "Push-ups",
+      "description": "تمرين الضغط لتقوية الصدر، الكتف، والذراعين.",
+      "video": "https://www.youtube.com/watch?v=_l3ySVKYVJ8",
+      "gif": "https://media.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+    },
+    {
+      "name": "Pull-ups",
+      "description": "تمرين السحب لتقوية الظهر والذراعين.",
+      "video": "https://www.youtube.com/watch?v=eGo4IYlbE5g",
+      "gif": "https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif"
+    },
+    {
+      "name": "Squats",
+      "description": "تمرين القرفصاء لتقوية الأرجل والعضلات الأساسية.",
+      "video": "https://www.youtube.com/watch?v=aclHkVaku9U",
+      "gif": "https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif"
+    },
+    {
+      "name": "Lunges",
+      "description": "تمرين الطعن للأرجل لتقوية الفخذين والمؤخرة.",
+      "video": "https://www.youtube.com/watch?v=COKYKgQ8KR0",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Plank",
+      "description": "تمرين اللوح لتقوية عضلات البطن والعمود الفقري.",
+      "video": "https://www.youtube.com/watch?v=B296mZDhrP4",
+      "gif": "https://media.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+    },
+    {
+      "name": "Dumbbell Rows",
+      "description": "تمرين السحب بالأوزان لتقوية الظهر والكتفين.",
+      "video": "https://www.youtube.com/watch?v=pYcpY20QaE8",
+      "gif": "https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif"
+    },
+    {
+      "name": "Overhead Press",
+      "description": "تمرين رفع الأوزان فوق الرأس لتقوية الكتف والذراعين.",
+      "video": "https://www.youtube.com/watch?v=2yjwXTZQDDI",
+      "gif": "https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif"
+    },
+    {
+      "name": "Bicep Curls",
+      "description": "تمرين ثني الذراع بالأوزان لتقوية العضلة ذات الرأسين.",
+      "video": "https://www.youtube.com/watch?v=ykJmrZ5v0Oo",
+      "gif": "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif"
+    },
+    {
+      "name": "Tricep Dips",
+      "description": "تمرين عضلة الترايسبس باستخدام كرسي أو أداة ثابتة.",
+      "video": "https://www.youtube.com/watch?v=0326dy_-CzM",
+      "gif": "https://media.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"
+    },
+    {
+      "name": "Deadlifts",
+      "description": "تمرين الرفع الأرضي لتقوية الظهر، الأرجل، والعضلات الأساسية.",
+      "video": "https://www.youtube.com/watch?v=op9kVnSso6Q",
+      "gif": "https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif"
+    }
+  ]
+};
