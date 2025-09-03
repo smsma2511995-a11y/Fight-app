418f6e522750bb2824a2d3ca7cfc70eeea092873
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-03 15:50:19 +0300

    Update martial_exercises.dart

--- lib/data/martial_exercises.dart
+++ lib/data/martial_exercises.dart
@@ -1,34 +1,36 @@
 import '../models/exercise_model.dart';
-import 'aikido_seed.dart';
-import 'bjj_seed.dart';
-import 'boxing_seed.dart';
 import 'karate_seed.dart';
 import 'judo_seed.dart';
-import 'kickboxing_seed.dart';
-import 'kungfu_seed.dart';
-import 'muaythai_seed.dart';
-import 'musclebuilding_seed.dart';
-import 'streetfighting_seed.dart';
 import 'taekwondo_seed.dart';
+import 'muaythai_seed.dart';
+import 'streetfighting_seed.dart';
+import 'musclebuilding_seed.dart';
 import 'weapondefense_seed.dart';
 import 'wrestling_seed.dart';
+import 'kungfu_seed.dart';
+import 'kickboxing_seed.dart';
+import 'boxing_seed.dart';
+import 'bjj_seed.dart';
+import 'aikido_seed.dart';
 import 'flexibility_seed.dart';
 import 'hipopening_seed.dart';
+import 'reaction_defense_seed.dart'; // ✅ الجديد
 
 final List<Exercise> allExercises = [
-  ...aikidoExercises,
-  ...bjjExercises,
-  ...boxingExercises,
   ...karateExercises,
   ...judoExercises,
-  ...kickboxingExercises,
-  ...kungfuExercises,
-  ...muaythaiExercises,
-  ...muscleBuildingExercises,
-  ...streetFightingExercises,
   ...taekwondoExercises,
+  ...muaythaiExercises,
+  ...streetFightingExercises,
+  ...muscleBuildingExercises,
   ...weaponDefenseExercises,
   ...wrestlingExercises,
+  ...kungfuExercises,
+  ...kickboxingExercises,
+  ...boxingExercises,
+  ...bjjExercises,
+  ...aikidoExercises,
   ...flexibilityExercises,
   ...hipOpeningExercises,
+  ...reactionDefenseExercises, // ✅ الجديد
 ];
