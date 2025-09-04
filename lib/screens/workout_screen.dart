bc6682c6f006843038a70d0fc061ef701715f690
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-04 08:58:41 +0300

    Update workout_screen.dart

--- lib/screens/workout_screen.dart
+++ lib/screens/workout_screen.dart
@@ -1,92 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import '../models/exercise_model.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import '../services/tts_service.dart';
+import 'exercise_detail_screen.dart';
 
-class WorkoutScreen extends StatefulWidget {
-  final Exercise exercise;
+class WorkoutScreen extends StatelessWidget {
+  final List<Exercise> exercises;
 
-  const WorkoutScreen({required this.exercise, super.key});
-
-  @override
-  State<WorkoutScreen> createState() => _WorkoutScreenState();
-}
-
-class _WorkoutScreenState extends State<WorkoutScreen> {
-  VideoPlayerController? _controller;
-  bool _isVideo = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadMedia();
-  }
-
-  Future<void> _loadMedia() async {
-    if (widget.exercise.video != null) {
-      _isVideo = true;
-      // تحميل الفيديو من الإنترنت مع Cache تلقائي
-      final file = await DefaultCacheManager().getSingleFile(widget.exercise.video!);
-      _controller = VideoPlayerController.file(file)
-        ..initialize().then((_) {
-          setState(() {});
-          _controller!.play();
-        });
-    } else if (widget.exercise.gif != null) {
-      _isVideo = false;
-      setState(() {}); // GIF سيظهر من الإنترنت
-    }
-  }
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
-  }
+  WorkoutScreen({required this.exercises});
 
   @override
   Widget build(BuildContext context) {
+    final tts = TTSService();
+
     return Scaffold(
-      appBar: AppBar(title: Text(widget.exercise.name)),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Container(
-              height: 250,
-              width: double.infinity,
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
-              child: _isVideo
-                  ? (_controller != null && _controller!.value.isInitialized
-                      ? AspectRatio(
-                          aspectRatio: _controller!.value.aspectRatio,
-                          child: VideoPlayer(_controller!),
-                        )
-                      : const Center(child: CircularProgressIndicator()))
-                  : widget.exercise.gif != null
-                      ? Image.network(widget.exercise.gif!, fit: BoxFit.cover)
-                      : const Icon(Icons.sports_mma_outlined, size: 80, color: Colors.grey),
-            ),
-            const SizedBox(height: 16),
-            Text('ابدأ التمرين الآن', style: Theme.of(context).textTheme.headline6),
-            const SizedBox(height: 8),
-            ElevatedButton(
-              onPressed: () {
-                if (_isVideo && _controller != null) {
-                  if (_controller!.value.isPlaying) {
-                    _controller!.pause();
-                  } else {
-                    _controller!.play();
-                  }
-                  setState(() {});
-                }
+      appBar: AppBar(
+        title: Text("تمارين اليوم"),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: ListView.builder(
+        itemCount: exercises.length,
+        itemBuilder: (context, index) {
+          final ex = exercises[index];
+          return Card(
+            margin: EdgeInsets.all(10),
+            child: ListTile(
+              leading: CachedNetworkImage(
+                imageUrl: ex.imageUrl,
+                placeholder: (ctx, url) => CircularProgressIndicator(),
+                errorWidget: (ctx, url, err) => Icon(Icons.error),
+                width: 60,
+                height: 60,
+                fit: BoxFit.cover,
+              ),
+              title: Text(ex.name),
+              subtitle: Text(ex.description, maxLines: 1, overflow: TextOverflow.ellipsis),
+              trailing: IconButton(
+                icon: Icon(Icons.volume_up, color: Colors.deepPurple),
+                onPressed: () {
+                  tts.speak(ex.description);
+                },
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => ExerciseDetailScreen(exercise: ex),
+                  ),
+                );
               },
-              child: Text(_isVideo
-                  ? (_controller != null && _controller!.value.isPlaying ? 'إيقاف الفيديو' : 'تشغيل الفيديو')
-                  : 'تمرين GIF'),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
