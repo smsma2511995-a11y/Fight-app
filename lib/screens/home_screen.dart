04dbfc0d52b0201b66417ef7716f5336261337a5
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-04 09:59:41 +0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,84 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path/path.dart';
 import 'package:url_launcher/url_launcher.dart';
+import '../helpers/database_helper.dart';
+import '../models/exercise.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'تطبيق التمارين',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomeScreen(),
-    );
-  }
-}
-
-// ------------------ Database Helper ------------------
-class DatabaseHelper {
-  static final DatabaseHelper _instance = DatabaseHelper._internal();
-  factory DatabaseHelper() => _instance;
-  DatabaseHelper._internal();
-
-  static Database? _database;
-
-  Future<Database> get database async {
-    if (_database != null) return _database!;
-    _database = await _initDB();
-    return _database!;
-  }
-
-  Future<Database> _initDB() async {
-    String path = join(await getDatabasesPath(), 'exercises.db');
-    return await openDatabase(
-      path,
-      version: 1,
-      onCreate: (db, version) async {
-        await db.execute('''
-          CREATE TABLE exercises(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            description TEXT,
-            videoUrl TEXT
-          )
-        ''');
-      },
-    );
-  }
-
-  Future<List<Map<String, dynamic>>> getExercises() async {
-    final db = await database;
-    return await db.query('exercises', orderBy: 'id DESC');
-  }
-
-  Future<int> insertExercise(Map<String, dynamic> exercise) async {
-    final db = await database;
-    return await db.insert('exercises', exercise);
-  }
-
-  Future<int> deleteExercise(int id) async {
-    final db = await database;
-    return await db.delete('exercises', where: 'id = ?', whereArgs: [id]);
-  }
-
-  Future<int> deleteAll() async {
-    final db = await database;
-    return await db.delete('exercises');
-  }
-}
-
-// ------------------ Home Screen ------------------
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Map<String, dynamic>> _exercises = [];
+  List<Exercise> _exercises = [];
 
   @override
   void initState() {
@@ -88,17 +19,15 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void _loadExercises() async {
     final data = await DatabaseHelper().getExercises();
-    setState(() {
-      _exercises = data;
-    });
+    setState(() { _exercises = data; });
   }
 
   void _addDummyExercise() async {
-    await DatabaseHelper().insertExercise({
-      'name': 'تمرين جديد',
-      'description': 'وصف التمرين هنا',
-      'videoUrl': 'https://www.youtube.com/', // ضع رابط فيديو حقيقي لو حابب
-    });
+    await DatabaseHelper().insertExercise(Exercise(
+      name: 'تمرين جديد',
+      description: 'وصف التمرين هنا',
+      videoUrl: 'https://www.youtube.com/',
+    ));
     _loadExercises();
   }
 
@@ -112,41 +41,28 @@ class _HomeScreenState extends State<HomeScreen> {
     _loadExercises();
   }
 
-  void _showDetails(Map<String, dynamic> exercise) {
+  void _showDetails(Exercise ex) {
     showDialog(
       context: context,
       builder: (_) => AlertDialog(
-        title: Text(exercise['name']),
+        title: Text(ex.name),
         content: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text(exercise['description']),
+            Text(ex.description),
             SizedBox(height: 10),
-            if (exercise['videoUrl'] != null && exercise['videoUrl'] != '')
-              ElevatedButton.icon(
-                icon: Icon(Icons.play_arrow),
-                label: Text('تشغيل الفيديو'),
-                onPressed: () async {
-                  final url = exercise['videoUrl'];
-                  if (await canLaunch(url)) {
-                    await launch(url);
-                  }
-                },
-              ),
+            if (ex.videoUrl != '') ElevatedButton.icon(
+              icon: Icon(Icons.play_arrow),
+              label: Text('تشغيل الفيديو'),
+              onPressed: () async {
+                if (await canLaunch(ex.videoUrl)) await launch(ex.videoUrl);
+              },
+            ),
           ],
         ),
         actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text('إغلاق'),
-          ),
-          TextButton(
-            onPressed: () {
-              _deleteExercise(exercise['id']);
-              Navigator.pop(context);
-            },
-            child: Text('حذف'),
-          ),
+          TextButton(onPressed: () => Navigator.pop(context), child: Text('إغلاق')),
+          TextButton(onPressed: () { _deleteExercise(ex.id!); Navigator.pop(context); }, child: Text('حذف')),
         ],
       ),
     );
@@ -158,45 +74,29 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('الرئيسية'),
         actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: _addDummyExercise,
-          ),
-          IconButton(
-            icon: Icon(Icons.delete_sweep),
-            onPressed: _deleteAllExercises,
-          ),
+          IconButton(icon: Icon(Icons.add), onPressed: _addDummyExercise),
+          IconButton(icon: Icon(Icons.delete_sweep), onPressed: _deleteAllExercises),
         ],
       ),
       body: _exercises.isEmpty
           ? Center(child: Text('لا يوجد تمارين بعد'))
           : Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: EdgeInsets.all(8.0),
               child: GridView.builder(
                 itemCount: _exercises.length,
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 3 / 2,
-                  crossAxisSpacing: 8,
-                  mainAxisSpacing: 8,
+                  crossAxisCount: 2, childAspectRatio: 3/2, crossAxisSpacing: 8, mainAxisSpacing: 8
                 ),
                 itemBuilder: (context, index) {
-                  final exercise = _exercises[index];
+                  final ex = _exercises[index];
                   return GestureDetector(
-                    onTap: () => _showDetails(exercise),
+                    onTap: () => _showDetails(ex),
                     child: Card(
                       color: Colors.blueAccent,
                       child: Center(
                         child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            exercise['name'],
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold),
-                            textAlign: TextAlign.center,
-                          ),
+                          padding: EdgeInsets.all(8),
+                          child: Text(ex.name, textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
                         ),
                       ),
                     ),
@@ -206,4 +106,4 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
     );
   }
-} 
+}
