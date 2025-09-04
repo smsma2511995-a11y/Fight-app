007b3740433be98c50bf6db5f1d656dedf8fa778
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-04 09:41:03 +0300

    Create tts_service.dart

---
+++ lib/services/tts_service.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_tts/flutter_tts.dart';
+
+class TTSService {
+  final FlutterTts _tts = FlutterTts();
+
+  Future<void> speak(String text) async {
+    await _tts.setLanguage("ar-SA"); // عربي
+    await _tts.setPitch(1.0);
+    await _tts.speak(text);
+  }
+
+  Future<void> stop() async {
+    await _tts.stop();
+  }
+}
