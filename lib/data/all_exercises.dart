54b517750ad59d17448242de83f889f8557602c5
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-04 17:10:34 +0300

    Update all_exercises.dart

--- lib/data/all_exercises.dart
+++ lib/data/all_exercises.dart
@@ -1,6 +1,6 @@
-// Alias file: expose martial_exercises.allExercises as allExercisesList
+// Alias: expose martial_exercises.allExercises as allExercisesList
+// يساعد على توافق الاستيرادات القديمة مع الاسم new allExercisesList
 import 'martial_exercises.dart' as _m;
 import '../models/exercise_model.dart';
 
-// Ensure we always expose a List<Exercise> named allExercisesList
 final List<Exercise> allExercisesList = List<Exercise>.from(_m.allExercises);
