d879619ad45c14958277b1493c4df1ef39bfc567
smsma2511995-a11y <smsma2511995@gmail.com>
2025-09-04 09:32:04 +0300

    Update exercise_model.dart

--- lib/models/exercise_model.dart
+++ lib/models/exercise_model.dart
@@ -3,33 +3,29 @@ class Exercise {
   final String name;
   final String description;
   final String videoUrl;
-  final String? localPath; // الملف بعد التخزين
+  final String? cachedPath;
 
   Exercise({
     this.id,
     required this.name,
     required this.description,
     required this.videoUrl,
-    this.localPath,
+    this.cachedPath,
   });
 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'description': description,
-      'videoUrl': videoUrl,
-      'localPath': localPath,
-    };
-  }
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'name': name,
+        'description': description,
+        'videoUrl': videoUrl,
+        'cachedPath': cachedPath,
+      };
 
-  factory Exercise.fromMap(Map<String, dynamic> map) {
-    return Exercise(
-      id: map['id'],
-      name: map['name'],
-      description: map['description'],
-      videoUrl: map['videoUrl'],
-      localPath: map['localPath'],
-    );
-  }
+  factory Exercise.fromMap(Map<String, dynamic> map) => Exercise(
+        id: map['id'],
+        name: map['name'],
+        description: map['description'],
+        videoUrl: map['videoUrl'],
+        cachedPath: map['cachedPath'],
+      );
 }
